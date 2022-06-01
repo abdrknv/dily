0990d623f898c94929160f63a488c474828260c5
abdrknv <ruslanabdarkunov@gmail.com>
2022-06-01 19:49:06 +0600

    first add

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from "./Banner/Banner";
+import Slider from "./Slider/Slider";
+
+const Home = () => {
+    return (
+        <main>
+            <Banner/>
+            <Slider/>
+        </main>
+    );
+};
+
+export default Home;
